eca7282a9871809eacfb2a2a8ccc89baa2052617
Sinead Bruce <2887574b@student.gla.ac.uk>
2025-01-30 20:02:50 +0000

    third commit

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,13 @@ from django.http import HttpResponse
 def index(request): # this is a view called index - views take one argument:
                     # a HttpResponse object called "request".
                     # Said object takes string param representing content of page
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Rango says hey there partner! <br>"
+        "<a href = 'http://127.0.0.1:8000/rango/about/'>About Page</a>"
+        )
+
+def about(request):
+    return HttpResponse(
+        "<a href = 'http://127.0.0.1:8000/'>Home</a> <br>"
+        "Rango says here is the about page."
+        )
