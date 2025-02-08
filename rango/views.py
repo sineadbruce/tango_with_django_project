ffc510f17f0650b4bcf5561989c61187455540e9
Sinead Bruce <2887574b@student.gla.ac.uk>
2025-02-08 16:08:40 +0000

    fifth

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+#from rango.models import Category, Page
+
 def index(request):
 
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
