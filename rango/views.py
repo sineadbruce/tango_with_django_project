e914b5eba64dd02d71a2ca07ff5c897b693930c5
Sinead Bruce <2887574b@student.gla.ac.uk>
2025-02-08 13:05:36 +0000

    fourth

--- rango/views.py
+++ rango/views.py
@@ -2,16 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request): # this is a view called index - views take one argument:
-                    # a HttpResponse object called "request".
-                    # Said object takes string param representing content of page
-    return HttpResponse(
-        "Rango says hey there partner! <br>"
-        "<a href = 'http://127.0.0.1:8000/rango/about/'>About Page</a>"
-        )
+def index(request):
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse(
-        "<a href = 'http://127.0.0.1:8000/'>Home</a> <br>"
-        "Rango says here is the about page."
-        )
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Sinead.'}
+    
+    return render(request, 'rango/about.html', context=context_dict)
