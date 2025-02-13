ccdd57f93b283fe6776303633708494460fdc943
Sinead Bruce <2887574b@student.gla.ac.uk>
2025-02-13 12:10:51 +0000

    eigth

--- rango/urls.py
+++ rango/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from rango import views
 
 app_name = 'rango'
@@ -10,4 +11,6 @@ urlpatterns = [
          views.show_category, name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+    path('category/<slug:category_name_slug>/add_page/',
+         views.add_page, name='add_page'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -22,6 +22,9 @@ def index(request):
 def about(request):
 
     context_dict = {'boldmessage': 'This tutorial has been put together by Sinead.'}
+
+    print(request.method)
+    print(request.user)
     
     return render(request, 'rango/about.html', context=context_dict)
 
